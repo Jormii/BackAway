e943d18dde505033a84875800105120dde176511
Jorge <lopeznatalj@gmail.com>
2022-10-08 20:36:17 +0200

    Basic individual game state structure

--- include/game_state.h
+++ include/game_state.h
@@ -5,11 +5,19 @@
 #include "sprite.h"
 #include "physics.h"
 
+typedef enum GameStateID_en
+{
+    GAME_STATE_MAIN_MENU,
+    GAME_STATE_LEVEL,
+    _GAME_STATE_N_STATES_
+} GameStateID;
+
 struct Level_st;
 struct Player_st;
 typedef struct GameState_st
 {
     float delta;
+    GameStateID state_id;
     Vec2 camera_focus;
     Vec2 camera_half_extension;
 
@@ -17,7 +25,7 @@ typedef struct GameState_st
     struct Player_st *player;
 } GameState;
 
-void game_state_init(GameState *game_state);
+void game_state_init(GameState *game_state, GameStateID initial_state);
 void game_state_update(GameState *game_state, float delta);
 
 void game_state_load_level(GameState *game_state, struct Level_st *level);
--- src/game_state.c
+++ src/game_state.c
@@ -5,9 +5,30 @@
 #include "game_state.h"
 #include "screen_buffer.h"
 
-void game_state_init(GameState *game_state)
+typedef void (*GameStateUpdate_fp)(GameState *game_state);
+typedef void (*GameStateDraw_fp)(const GameState *game_state);
+
+typedef struct GameStateCallbacks_st
+{
+    GameStateUpdate_fp update_cb;
+    GameStateDraw_fp draw_cb;
+} GameStateCallbacks;
+
+void menu_state_update(GameState *game_state);
+void menu_state_draw(const GameState *game_state);
+
+void level_state_update(GameState *game_state);
+void level_state_draw(const GameState *game_state);
+
+GameStateCallbacks state_cbs[_GAME_STATE_N_STATES_] = {
+    {.update_cb = menu_state_update, .draw_cb = menu_state_draw},
+    {.update_cb = level_state_update, .draw_cb = level_state_draw},
+};
+
+void game_state_init(GameState *game_state, GameStateID initial_state)
 {
     game_state->delta = 0.0f;
+    game_state->state_id = initial_state;
     game_state->camera_focus.x = 0.0f;
     game_state->camera_focus.y = 0.0f;
     game_state->camera_half_extension.x = 0.5 * SCREEN_WIDTH;
@@ -53,13 +74,9 @@ void game_state_update(GameState *game_state, float delta)
 {
     game_state->delta = delta;
 
-    // Update
-    level_update(game_state->level, game_state);
-    player_update(game_state->player, game_state);
-
-    // Draw
-    level_draw(game_state->level, game_state);
-    player_draw(game_state->player, game_state);
+    GameStateCallbacks cbs = state_cbs[game_state->state_id];
+    cbs.update_cb(game_state);
+    cbs.draw_cb(game_state);
 }
 
 void game_state_load_level(GameState *game_state, struct Level_st *level)
@@ -77,4 +94,24 @@ Vec2 game_state_camera_transform(const GameState *game_state, const Vec2 *positi
 {
     Vec2 origin = vec2_subtract(&(game_state->camera_focus), &(game_state->camera_half_extension));
     return vec2_subtract(position, &origin);
+}
+
+void menu_state_update(GameState *game_state)
+{
+}
+
+void menu_state_draw(const GameState *game_state)
+{
+}
+
+void level_state_update(GameState *game_state)
+{
+    level_update(game_state->level, game_state);
+    player_update(game_state->player, game_state);
+}
+
+void level_state_draw(const GameState *game_state)
+{
+    level_draw(game_state->level, game_state);
+    player_draw(game_state->player, game_state);
 }
